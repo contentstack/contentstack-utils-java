e20939bb9409abe89539ddb97fff962e7eb55d41
Shailesh Mishra <mshaileshr@gmail.com>
2022-03-29 11:17:16 +0530

    scope ignored

--- src/main/java/com/contentstack/utils/presets/Preset.java
+++ src/main/java/com/contentstack/utils/presets/Preset.java
@@ -14,6 +14,7 @@ import static com.contentstack.utils.presets.Constant.*;
 
 public class Preset {
 
+
     public String resolvePresetByPresetName(
             @NotNull JSONObject asset,
             @NotNull String extensionUid,
